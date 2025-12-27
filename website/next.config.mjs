b3caf8ee159d29e850506a74470b584fcbf32a28
pavanpodila <pavan@vyuh.tech>
2025-12-27 05:44:30 +0530

    feat: add Cloudflare configuration and support for package transpilation

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -5,7 +5,7 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
-  output: 'export',
+  transpilePackages: ['shiki', '@shikijs/core', '@shikijs/engine-javascript'],
 };
 
 export default withMDX(config);
